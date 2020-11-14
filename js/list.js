6370678ce8a498a530331e63fd22529cfac9c469
YeEeck <yechenkai@foxmail.com>
2020-11-14 10:44:11 +0800

    update20201114

---
+++ js/list.js
@@ -0,0 +1,44 @@
+var normal;
+
+window.onload = function(){
+	var t = document.getElementsByClassName("examListElem");
+	normal = t[0].style.display;
+};
+
+function change_display(str) {
+	var t = document.getElementsByClassName("examListElem");
+	for (var i = 0; i < t.length; i++) {
+		t[i].style.display = "none";
+	}
+	if (str == 'dt') {
+		var t = document.getElementsByClassName("dtExam");
+		for (var i = 0; i < t.length; i++) {
+			t[i].style.display = normal;
+		}
+	} else if(str == 'all'){
+		var t = document.getElementsByClassName("examListElem");
+		for (var i = 0; i < t.length; i++) {
+			t[i].style.display = normal;
+		}
+	
+	} else if(str == 'kh'){
+		var t = document.getElementsByClassName("examListElem");
+		for (var i = 0; i < t.length; i++) {
+			t[i].style.display = "none";
+		}
+		var t = document.getElementsByClassName("khExam");
+		for (var i = 0; i < t.length; i++) {
+			t[i].style.display = normal;
+		}
+	} else if(str == 'qm'){
+		var t = document.getElementsByClassName("examListElem");
+		for (var i = 0; i < t.length; i++) {
+			t[i].style.display = "none";
+		}
+		var t = document.getElementsByClassName("qmExam");
+		for (var i = 0; i < t.length; i++) {
+			t[i].style.display = normal;
+		}
+	}
+
+}
